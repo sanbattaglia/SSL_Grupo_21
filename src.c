099558cbe02e4a968a1ed7b26ec1c6cbf55fce57
sanbattaglia <sbattaglia@frba.utn.edu.ar>
2025-05-29 16:45:41 -0300

    Primeros pasos...

---
+++ src.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+typedef struct {
+   char terminales[10];
+   char noTerminales[10];
+   
+} gramatica;
+
+
+int main(){
+
+    gramatica grIngresada;
+}

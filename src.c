f05cbd673867bd08fe1a0feeb950065dff2c35df
sanbattaglia <sbattaglia@frba.utn.edu.ar>
2025-05-29 16:46:37 -0300

    Primeros pasos

--- src.c
+++ src.c
@@ -13,4 +13,16 @@ typedef struct {
 int main(){
 
     gramatica grIngresada;
+
+    printf("Ingrese los simbolos terminales (sin espacios, ej: abc): ");
+    scanf("%s", grIngresada.terminales);
+
+    printf("Ingrese los simbolos no terminales (sin espacios, ej: ST): ");
+    scanf("%s", grIngresada.noTerminales);
+
+    // Mostrar lo ingresado para verificar
+    printf("\nTerminales ingresados: %s\n", grIngresada.terminales);
+    printf("No terminales ingresados: %s\n", grIngresada.noTerminales);
+
+    return 0;
 }

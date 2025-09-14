6673f29de81d685498bfdebe1d305597d0be36c6
sanbattaglia <sbattaglia@frba.utn.edu.ar>
2025-09-14 13:57:06 -0300

    Punto a)

--- TP2/main.c
+++ TP2/main.c
@@ -1,2 +1,46 @@
 /* Arranco TP2 */
 
+#include <stdio.h>
+#include <ctype.h>
+
+#define MAX_LINE 300
+
+int contar_oraciones(FILE *archivo) {
+    
+    char linea[MAX_LINE];
+    int oraciones = 0;
+    
+    fseek(archivo, 0, SEEK_SET);
+    
+    while (fgets(linea, MAX_LINE, archivo) != NULL) {
+        
+        for (int i = 0; linea[i] != '\0'; i++) {
+            if (linea[i] == '.' || linea[i] == '!' || linea[i] == '?') {
+                
+                if (linea[i + 1] == '\0' || linea[i + 1] == '\n' || 
+                    linea[i + 1] == ' ' || isupper(linea[i + 2])) {
+                    oraciones++;
+                }
+            }
+        }
+    }
+    
+    return oraciones;
+}
+
+int main() {
+    
+    FILE *archivo = fopen("breve_historia.txt", "r");
+    
+    if (archivo == NULL) {
+        printf("Error al abrir el archivo\n");
+        return 1;
+    }
+    
+    int total_oraciones = contar_oraciones(archivo);
+    printf("a) Cantidad de oraciones procesadas: %d\n", total_oraciones);
+    
+    fclose(archivo);
+    
+    return 0;
+}
